e8de3391a3ab04b530485203308b5d7655baf9c0
Keith Kurak <keith.kurak@gmail.com>
2025-05-06 18:57:26 -0400

    wrong lockfile

--- components/EditWorkoutSheet.tsx
+++ components/EditWorkoutSheet.tsx
@@ -14,6 +14,7 @@ import {
   Pressable,
   ScrollView,
   Dimensions,
+  Platform,
 } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import BottomSheet, {
@@ -185,7 +186,7 @@ const EditWorkoutSheet = ({
       onClose={onClose}
       backdropComponent={renderBackdrop}
       handleIndicatorStyle={styles.bottomSheetIndicator}
-      enableContentPanningGesture={true}
+      enableContentPanningGesture={Platform.OS === "ios" ? false : false}
     >
       <BottomSheetView style={styles.container}>
         <ScrollView showsVerticalScrollIndicator={false}>
